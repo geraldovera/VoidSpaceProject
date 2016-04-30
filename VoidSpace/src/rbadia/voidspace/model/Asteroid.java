1d2689fc3af903e9aee835fd4bfd23f4bd9d4b2b
geraldovera <geraldo.vera@upr.edu>
2016-04-30 15:23:43 -0400

    First attempt at commiting local repositorie

---
+++ VoidSpace/src/rbadia/voidspace/model/Asteroid.java
@@ -0,0 +1,68 @@
+package rbadia.voidspace.model;
+
+import java.awt.Rectangle;
+import java.util.Random;
+
+import rbadia.voidspace.main.GameScreen;
+
+public class Asteroid extends Rectangle {
+	private static final long serialVersionUID = 1L;
+	
+	public static final int DEFAULT_SPEED = 4;
+	
+	private int asteroidWidth = 32;
+	private int asteroidHeight = 32;
+	private int speed = DEFAULT_SPEED;
+
+	private Random rand = new Random();
+	private int dir;
+
+	
+	/**
+	 * Crates a new asteroid at a random x location at the top of the screen 
+	 * @param screen the game screen
+	 */
+	public Asteroid(GameScreen screen){
+		this.setLocation(
+        		rand.nextInt(screen.getWidth() - asteroidWidth),
+        		0);
+		this.setSize(asteroidWidth, asteroidHeight);
+	}
+	
+	public void newDirection(){
+		dir = (int) (rand.nextInt(4) * Math.pow(-1, rand.nextInt(2)));
+	}
+	public int getAsteroidDirection(){
+		return dir;
+	}
+	public int getAsteroidWidth() {
+		return asteroidWidth;
+	}
+	public int getAsteroidHeight() {
+		return asteroidHeight;
+	}
+
+	/**
+	 * Returns the current asteroid speed
+	 * @return the current asteroid speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+	
+	/**
+	 * Set the current asteroid speed
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	/**
+	 * Returns the default asteroid speed.
+	 * @return the default asteroid speed
+	 */
+	public int getDefaultSpeed(){
+		return DEFAULT_SPEED;
+	}
+}
